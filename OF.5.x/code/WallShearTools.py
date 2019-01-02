da20429b2534b577c1ba83f7ded91e00aa08c96b
plgrover <patrick.l.grover@gmail.com>
2019-01-02 20:25:24 +0000

    Added some code to develop vertical shear stress profiles

---
+++ OF.5.x/code/WallShearTools.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+'''
+This library is designed to help prepare plots for showing the near-wall shear stresses
+'''
+
+__author__ = "Patrick Grover"
+__copyright__ = ""
+__credits__ = ["Patrick Grover"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Patrick Grover"
+__email__ = ""
+__status__ = "Development"
+
+import pandas as pd
+
+
